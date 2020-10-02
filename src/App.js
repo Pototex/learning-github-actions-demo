eb6767ac6789d42892b9d9f23d19f985369d1b14
Gustavo Matarrita Madrigal <Pototo@Ericks-MacBook-Pro.local>
2020-10-02 12:48:04 -0600

    Test CI/CD

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
 import './App.css';
 
-function App() {
+const TitleComponent = React.memo(({index, children, sum, hello}) => {
+  const [color, setColor] = React.useState('#ffffff')
+
+  const handleColorChange = () => {
+    setColor(color === '#ffffff' ? '#f5b5d3' : '#ffffff')
+  }
+
+  return (
+    <div 
+      className="Title-box"
+      type="button"
+      style={{ backgroundColor: color}}
+      onClick={() => handleColorChange()}
+    >
+      <span>{hello} {sum(index)}</span>
+    </div>
+  )
+})
+
+export default function App() {
+  const [titles, setTitles] = React.useState([])
+  const [order, setOrder] = React.useState(false)
+  
+  React.useEffect(
+    () => {
+      let array = new Array(45)
+      for(let index = 0; index <= 45; index++) {
+        array[index] = index
+      }
+      setTitles(array)
+    },
+    []
+  )
+
+  const reverse = () => {
+    const sorted = titles.sort((a, b) => order ? a - b : b - a)
+    setTitles(sorted)
+    setOrder(!order)
+  } 
+
+  const sum = (index) => {
+    return index + 1
+  }
+
+  const memoSum = useCallback(sum, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        titles.map((value) => (
+          <TitleComponent
+            key={`title-${value}`} 
+            index={value}
+            sum={memoSum}
+            hello={`Dummy`}>
+          </TitleComponent>
+        ))
+      }
+      <div>
+        <button onClick={() => reverse()}>Change order</button>
+      </div>
     </div>
   );
 }
-
-export default App;
